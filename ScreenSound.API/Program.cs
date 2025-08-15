9496d39166fe9b275a80914bbd0d95d1d48de2b8
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-15 15:50:14 -0300

    Swagger - erro de versão

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -5,10 +5,29 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//REGISTRO DE SERVIÇOES
 //INJEÇÃO DE DEPENDÊNCIA - DAL<Artista>, DAL<Musica> E ScreenSoundContext
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
+
+//SWAGGER
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "ScreenSound API",
+        Version = "v1",
+        Description = "Uma API para gerenciamento de artistas e músicas",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "ScreenSound Support",
+            Email = "support@screensound.com",
+            Url = new Uri("https://screensound.com/support")
+        }
+    });
+});
 
 //IGNORAR CICLOS NA SERIALIZAÇÃO
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>
@@ -20,5 +39,9 @@ var app = builder.Build();
 //USO MÉTODO DE EXTENSÃO
 app.AddEndPointsArtistas();
 app.AddEndPointsMusicas();
+
+//USO SWAGGER
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
