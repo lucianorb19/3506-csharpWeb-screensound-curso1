9d76475580c094bc8ed2cf52a2a191e9cd3261fd
lucianorb19 <52081214+lucianorb19@users.noreply.github.com>
2025-08-15 09:48:53 -0300

    CRUD - Artistas
    Com uso de injeção de dependência para DAL<Artista> e ScreenSoundContext

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -4,6 +4,11 @@ using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//INJEÇÃO DE DEPENDÊNCIA - DAL<Artista> E ScreenSoundContext
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 //IGNORAR CICLOS NA SERIALIZAÇÃO
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>
     (options => options.SerializerOptions.ReferenceHandler = 
@@ -13,16 +18,15 @@ var app = builder.Build();
 
 
 
-app.MapGet("/Artistas", () =>
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     return Results.Ok(dal.Listar());
 });
 
 //LISTA TODOS ARTISTAS COM NOME ESPECÍFICO
-app.MapGet("/Artistas/{nome}", (string nome) =>
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL < Artista > dal, 
+                                string nome) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     var artista =  dal.RecuperarMuitosPor(
         a => a.Nome.ToUpper().Equals(nome.ToUpper()));
     if(artista is null)
@@ -33,11 +37,49 @@ app.MapGet("/Artistas/{nome}", (string nome) =>
 });
 
 //ADICIONA UM ARTISTA RECEBIDO NO CORPO DA REQUISIÇÃO
-app.MapPost("/Artistas",([FromBody]Artista artista) =>
+app.MapPost("/Artistas",([FromServices] DAL < Artista > dal,
+                         [FromBody]Artista artista) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
     dal.Adicionar(artista);
     return Results.Ok();
+});
+
+
+app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal,
+                                 int id) =>
+{
+    var artista = dal.RecuperarPrimeiroPor(a => a.Id.Equals(id));
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(artista);
+    return Results.NoContent();
+});
+
+
+app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal,
+                         [FromBody] Artista artista) =>
+{
+    var artistaAtualizado = dal.RecuperarPrimeiroPor(
+        a => a.Id.Equals(artista.Id));
+
+    if(artistaAtualizado is null)
+    {
+        return Results.NotFound();
+    }
+    artistaAtualizado.Nome = artista.Nome;
+    artistaAtualizado.Bio = artista.Bio;
+    artistaAtualizado.FotoPerfil = artista.FotoPerfil;
+    dal.Atualizar(artistaAtualizado);
+
+    return Results.Ok();
+
+    /*
+     artista [FromBody] -> artistaAtualizado <-> artista no BD
+     */
+
+
 });
 
 app.Run();
